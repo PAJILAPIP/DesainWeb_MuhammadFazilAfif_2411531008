be99716559f37726bfbcabd585e8e2cd8dad2ac0
PAJILAPIP <muhammadfazil2406@gmail.com>
2025-10-06 19:30:42 +0700

    Desain Web

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("./service-worker.js")
-      .then((reg) => console.log("Service Worker registered", reg))
-      .catch((err) => console.log("Service Worker registration failed", err));
-  });
-}
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", () => {
+      navigator.serviceWorker
+        .register("./service-worker.js")
+        .then((reg) => console.log("Service Worker registered", reg))
+        .catch((err) => console.log("Service Worker registration failed", err));
+    });
+  }
 
-let deferredPrompt;
+  let deferredPrompt;
 
-window.addEventListener("beforeinstallprompt", (e) => {
-  e.preventDefault();
-  deferredPrompt = e;
-  const installBtn = document.createElement("button");
-  installBtn.innerText = "Install PWA";
-  document.body.appendChild(installBtn);
+  window.addEventListener("beforeinstallprompt", (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    const installBtn = document.createElement("button");
+    installBtn.innerText = "Install PWA";
+    document.body.appendChild(installBtn);
 
-  installBtn.addEventListener("click", () => {
-    deferredPrompt.prompt();
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === "accepted") {
-        console.log("User accepted the install prompt");
-      }
-      deferredPrompt = null;
-      installBtn.remove();
+    installBtn.addEventListener("click", () => {
+      deferredPrompt.prompt();
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === "accepted") {
+          console.log("User accepted the install prompt");
+        }
+        deferredPrompt = null;
+        installBtn.remove();
+      });
     });
   });
-});
 
-window.addEventListener("appinstalled", () => {
-  console.log("PWA installed");
-});
+  window.addEventListener("appinstalled", () => {
+    console.log("PWA installed");
+  });
