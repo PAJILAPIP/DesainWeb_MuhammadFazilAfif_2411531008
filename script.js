83e6cf84be397620aa916dd642356841c8544d1f
PAJILAPIP <muhammadfazil2406@gmail.com>
2025-10-06 15:26:16 +0700

    Desain Web

---
+++ script.js
@@ -0,0 +1,33 @@
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("./service-worker.js")
+      .then((reg) => console.log("Service Worker registered", reg))
+      .catch((err) => console.log("Service Worker registration failed", err));
+  });
+}
+
+let deferredPrompt;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  const installBtn = document.createElement("button");
+  installBtn.innerText = "Install PWA";
+  document.body.appendChild(installBtn);
+
+  installBtn.addEventListener("click", () => {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the install prompt");
+      }
+      deferredPrompt = null;
+      installBtn.remove();
+    });
+  });
+});
+
+window.addEventListener("appinstalled", () => {
+  console.log("PWA installed");
+});
