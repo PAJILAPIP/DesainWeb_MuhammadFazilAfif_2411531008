924002d36fef79636bef5c6fea1f027b3344f326
PAJILAPIP <muhammadfazil2406@gmail.com>
2025-10-13 00:25:58 +0700

    desain web

--- service-worker.js
+++ service-worker.js
@@ -1,57 +1,57 @@
-const CACHE_NAME = 'Fazil';
-const ASSETS_TO_CACHE = [
-  './',
-  'index.html',
-  'contact.html',
-  'about.html',
-  'offline.html',
-  'style.css',
-  'android-chrome-192x192.png',
-  'android-chrome-512x512.png'
-];
+// const CACHE_NAME = 'Fazil';
+// const ASSETS_TO_CACHE = [
+//   './',
+//   'index.html',
+//   'contact.html',
+//   'about.html',
+//   'offline.html',
+//   'style.css',
+//   'android-chrome-192x192.png',
+//   'android-chrome-512x512.png'
+// ];
 
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Cache dibuka');
-        return cache.addAll(ASSETS_TO_CACHE);
-      })
-  );
-  self.skipWaiting();
-});
+// self.addEventListener('install', (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then((cache) => {
+//         console.log('Cache dibuka');
+//         return cache.addAll(ASSETS_TO_CACHE);
+//       })
+//   );
+//   self.skipWaiting();
+// });
 
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            console.log('Menghapus cache lama:', cacheName);
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
+// self.addEventListener('activate', (event) => {
+//   event.waitUntil(
+//     caches.keys().then((cacheNames) => {
+//       return Promise.all(
+//         cacheNames.map((cacheName) => {
+//           if (cacheName !== CACHE_NAME) {
+//             console.log('Menghapus cache lama:', cacheName);
+//             return caches.delete(cacheName);
+//           }
+//         })
+//       );
+//     })
+//   );
+//   self.clients.claim();
+// });
 
 
-self.addEventListener("fetch", (event) => {
-  if (event.request.mode === "navigate") {
-    event.respondWith(
-      fetch(event.request)
-        .then((response) => response)
-        .catch(() => caches.match("./offline.html"))
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request).then((cachedResponse) => {
-        return cachedResponse || fetch(event.request);
-      })
-    );
-  }
-});
+// self.addEventListener("fetch", (event) => {
+//   if (event.request.mode === "navigate") {
+//     event.respondWith(
+//       fetch(event.request)
+//         .then((response) => response)
+//         .catch(() => caches.match("./offline.html"))
+//     );
+//   } else {
+//     event.respondWith(
+//       caches.match(event.request).then((cachedResponse) => {
+//         return cachedResponse || fetch(event.request);
+//       })
+//     );
+//   }
+// });
